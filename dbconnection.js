e15c659da38fe0afdb08b7316e84d37103f790ef
PichikuntlaSriCharan <pichikuntlasricharan@gmail.com>
2026-05-26 23:42:17 +0530

    Completed insert,read iwith node and mongodb

--- dbconnection.js
+++ dbconnection.js
@@ -6,7 +6,7 @@ const client = new MongoClient(dbConnectionUrl);
 
 let dbConnection=async()=>{
     await client.connect();
-    client.db("mongoDBProject_DataBase")
+    let db = client.db("mongoDBProject_DataBase");
     return db;
  }
  module.exports = {dbConnection};
--- index.js
+++ index.js
@@ -1,16 +1,30 @@
 let express = require("express")
-
+let {dbConnection} = require("./dbconnection")
 let app = express()
-
 app.use(express.json())
 
-
-app.get("/student-read", (req, res) => {
-    res.send("Student view API");
+app.get("/student-read",async (req, res) => {
+    let mydb = await dbConnection();
+    let studentCollection = mydb.collection("students")
+    let data = await studentCollection.find().toArray();
+    let resobj={
+        status:"success",
+        data:data,
+        msg:"data read successfully"
+    }
+    res.send(resobj);
 })
 
-app.post("/student-insert", (req, res) => {
-    res.send("Student insert API");
-})
+app.post("/student-insert",async (req, res) => {
+    let mydb = await dbConnection();
+    let studentCollection = mydb.collection("students");
+    let result = await studentCollection.insertOne(req.body);
+    let resobj = {
+        status: "success",
+        data: result,
+        msg: "data inserted successfully"
+    };
+    res.send(resobj);
+});
 
-app.listen(8000) 
+app.listen(8000);
