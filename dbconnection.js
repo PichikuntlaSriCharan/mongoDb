1436ecc1e609713627ba048dc138a3c243e1b051
PichikuntlaSriCharan <pichikuntlasricharan@gmail.com>
2026-05-26 23:10:27 +0530

    connect database with node

---
+++ dbconnection.js
@@ -0,0 +1,12 @@
+const { MongoClient } = require('mongodb');
+
+let dbConnectionUrl = 'mongodb://127.0.0.1:27017';
+
+const client = new MongoClient(dbConnectionUrl);
+
+let dbConnection=async()=>{
+    await client.connect();
+    client.db("mongoDBProject_DataBase")
+    return db;
+ }
+ module.exports = {dbConnection};
--- index.js
+++ index.js
@@ -4,21 +4,13 @@ let app = express()
 
 app.use(express.json())
 
+
 app.get("/student-read", (req, res) => {
     res.send("Student view API");
 })
 
-app.post("/Student-insert", async (req, res) => {
-    let mydb=await dbconnection();
-    let studentcollection=mydb.collection("studentcollection");
-    
-    let obj={
-        sName:req.body.sName,
-        sEmail:req.body.sEmail,
-    }
-
-    console.log(obj);
+app.post("/student-insert", (req, res) => {
     res.send("Student insert API");
 })
 
-app.listen("8000")
+app.listen(8000) 
