076cdf11e84b061f9b10f93c6da4f8f528d9b236
PichikuntlaSriCharan <pichikuntlasricharan@gmail.com>
2026-05-27 16:49:32 +0530

    Completed read, insert, delete, update operations

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 let express = require("express")
 let {dbConnection} = require("./dbconnection")
+const { ObjectId } = require("mongodb")
 let app = express()
 app.use(express.json())
 
@@ -15,10 +16,15 @@ app.get("/student-read",async (req, res) => {
     res.send(resobj);
 })
 
-app.post("/student-insert",async (req, res) => {
+app.put("/student-insert",async (req, res) => {
     let mydb = await dbConnection();
     let studentCollection = mydb.collection("students");
-    let result = await studentCollection.insertOne(req.body);
+    let {sName,sEmail} = req.body;
+    let obj = {
+        sName,
+        sEmail
+    }
+    let result = await studentCollection.insertOne(obj);
     let resobj = {
         status: "success",
         data: result,
@@ -26,5 +32,73 @@ app.post("/student-insert",async (req, res) => {
     };
     res.send(resobj);
 });
+
+app.delete("/student-delete/:id",async (req, res) => {
+    let {id} = req.params;
+    let mydb = await dbConnection();
+    let studentCollection = mydb.collection("students");
+    let result = await studentCollection.deleteOne({_id:new ObjectId(id)});
+    let resobj = {
+        status: "success",
+        data: result,
+        msg: "data deleted successfully"
+    };
+    res.send(resobj);
+});
+
+app.put("/student-update/:id", async (req, res) => {
+    try {
+        let {id} = req.params;
+        let {sName,sEmail} = req.body;
+        let obj = {};
+        if (sName != "" && sName != undefined && sName != null) {
+            obj["sName"] = sName;
+        }
+        if (sEmail != "" && sEmail != undefined && sEmail != null) {
+            obj["sEmail"] = sEmail;
+        }
+        let mydb = await dbConnection();
+        let studentCollection = mydb.collection("students");
+        let result = await studentCollection.updateOne(
+            { _id: new ObjectId(id) },
+            { $set: obj }
+        );
+        let resobj = {
+            status: "success",
+            data: result,
+            msg: "data updated successfully"
+        };
+        res.status(200).json(resobj);
+    } catch (error) {
+        res.status(500).json({
+            status: "failed",
+            msg: error.message
+        });
+    }
+});
+
+// app.put("/student-update/:id",async (req, res) => {
+//     let {id} = req.params;
+//     let {sName,sEmail} = req.body;
+
+//     let obj={};
+
+//     if(sName!=="" && sName!==undefined && sName!==null){
+//         obj['sName']=sName;
+//     }
+//     if(sEmail!=="" && sEmail!==undefined && sEmail!==null){
+//         obj['sEmail']=sEmail;
+//     }
+
+//     let mydb = await dbConnection();
+//     let studentCollection = mydb.collection("students");
+//     let result = await studentCollection.updateOne({_id:new ObjectId(id)},{$set:obj});
+//     let resobj = {
+//         status: "success",
+//         data: result,
+//         msg: "data updated successfully"
+//     };
+//     res.send(resobj);
+// });
 
 app.listen(8000);
